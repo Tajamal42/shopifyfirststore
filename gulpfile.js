15b4ccbb1a3a95ec1681bcabaae85737e4bea10e
Tajamal <tajamal.azan@gmail.com>
2022-08-30 14:09:40 +1000

    commit header background changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const themekit = require('@shopify/themekit');
 const sass = require('gulp-sass')(require('sass'));
+const clean = require('gulp-clean-css');
 
 gulp.task('sass', function() {
     return gulp.src('scss/global.scss')
     .pipe(sass())
+    .pipe(clean({ compatibility: 'ie11' }))
     .pipe(gulp.dest('assets'))
 });
 
